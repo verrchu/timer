eab0401c62c28f5a7269700ab73922eea94bdc42
Evgeniy Terekhin <e_terekhin@wargaming.net>
2021-10-26 08:31:37 +0300

    add http tracing

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ mod dao;
 mod domain;
 
 use axum::{Router, Server};
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().nest("/api", api::router());
+    tracing_subscriber::fmt::init();
+
+    tracing::debug!("STARTING");
+
+    let app = Router::new()
+        .nest("/api", api::router())
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
